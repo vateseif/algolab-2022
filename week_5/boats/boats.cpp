506374f4179c8c35b137f01801418aabcf4c9043
vateseif <vateseif@gmail.com>
2022-11-16 01:01:08 +0100

    boats 60. using pair instead of vector, faster but not enough for tc 3

--- week_5/boats/boats.cpp
+++ week_5/boats/boats.cpp
@@ -1,27 +1,21 @@
+///1
 #include<iostream>
 #include<vector>
 #include<algorithm>
 #include<limits>
 
-#define tarce(x) std::cerr << #x << " = " << x << std::endl;
+#define trace(x) std::cerr << #x << " = " << x << std::endl;
 
 int n;
-std::vector<std::vector<int>> rings;
+std::vector<std::pair<int, int>> rings;
 
-// sort using a custom function object
-struct {
-    bool operator()(std::vector<int> a, std::vector<int> b) const
-    {   
-        return a[1] < b[1];
-    }   
-} sortRings;
 
 bool greedy(int& delta, int& boats, int& i){
   const int ii = i;
   int min_e = std::numeric_limits<int>::max();
   for (int j=ii+1; j<n; j++){
-    if (delta <= rings[j][1]){
-      int e = std::max(rings[j][1], delta+rings[j][0]);
+    if (delta <= rings[j].first){
+      int e = std::max(rings[j].first, delta+rings[j].second);
       if (min_e > e){
         min_e = e;
         i = j;
@@ -37,14 +31,14 @@ bool greedy(int& delta, int& boats, int& i){
 void testcase(){
   std::cin >> n;
   rings.clear();
-  rings.resize(n, std::vector<int>(2));
+  rings.resize(n);
   for (int i=0; i<n; i++){
    int l, p;
    std::cin >> l >> p;
-   rings[i] = {l, p};
+   rings[i] = std::make_pair(p, l);
   }
 
-  std::sort(rings.begin(), rings.end(), sortRings);
+  std::sort(rings.begin(), rings.end());
 
   int delta = std::numeric_limits<int>::min();
   int boats = 0;
