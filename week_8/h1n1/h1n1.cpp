2cba8aeff4d3e208ecc78b5868fc5d54504863b3
vateseif <vateseif@gmail.com>
2022-12-09 03:55:26 +0100

    h1n1 80. faster but still too slow for tc  5

--- week_8/h1n1/h1n1.cpp
+++ week_8/h1n1/h1n1.cpp
@@ -5,34 +5,61 @@
 #include <unordered_map>
 #include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
 #include <CGAL/Delaunay_triangulation_2.h>
+#include <CGAL/Triangulation_face_base_with_info_2.h>
 
 #define trace(x) std::cerr << #x << " = " << x << std::endl;
 
+struct FaceDataStructure{
+  long max_d;
+  std::vector<long> edges_lenght;
+};
+
 typedef CGAL::Exact_predicates_inexact_constructions_kernel K;
-typedef CGAL::Delaunay_triangulation_2<K>  Triangulation;
-typedef Triangulation::Edge_iterator  Edge_iterator;
+typedef CGAL::Triangulation_vertex_base_2<K> Vb;
+typedef CGAL::Triangulation_face_base_with_info_2<FaceDataStructure,K> Fb;
+typedef CGAL::Triangulation_data_structure_2<Vb,Fb> Tds;
+typedef CGAL::Delaunay_triangulation_2<K, Tds>  Triangulation;
+
 typedef K::Point_2 P;
 typedef Triangulation::Face_handle FaceHandle;
 typedef std::unordered_map<FaceHandle, long> MaxDistanceMap;
+typedef std::unordered_map<FaceHandle, std::unordered_map<int, long>> EdgeLengthMap;
 
 Triangulation t;
 std::vector<P> infected;
 MaxDistanceMap distance, opened;
+EdgeLengthMap edge_length;
 
+void init(){
+
+  for (FaceHandle f:t.finite_face_handles()){
+    FaceDataStructure fds;
+    fds.max_d = 0;
+    fds.edges_lenght.resize(3, 0);
+    for (int i=0;i<3;i++){
+      int k = f->cw(i);
+      int j = f->ccw(i);
+      P a = f->vertex(k)->point();
+      P b = f->vertex(j)->point();
+      long ab_distance = CGAL::squared_distance(a, b);
+      fds.edges_lenght[i] = ab_distance;
+    }
+    f->info() = fds;
+  }
+}
 
 bool find_exit(FaceHandle& face, const long& d){
   opened[face] = 1;
   if (t.is_infinite(face)) return true;
-  if (distance[face] >= d) return true;
+  
+  FaceDataStructure fds = face->info();
+  if (fds.max_d >= d) return true;
   for (int i=0;i<3;i++){
     FaceHandle neighbor_face = face->neighbor(i);
     if (opened[neighbor_face]) continue;
-    P a = face->vertex(face->ccw(i))->point();
-    P b = face->vertex(face->cw(i))->point();
-    long ab_distance = CGAL::squared_distance(a, b);
-
+    long ab_distance = fds.edges_lenght[i];
     if (ab_distance >= 4*d && find_exit(neighbor_face, d)){
-      distance[face] = d;
+      face->info().max_d = d;
       return true;
     }
   }
@@ -64,9 +91,11 @@ bool testcase(){
     std::cin >> x >> y;
     infected[i] = P(x, y);
   }
-
   t.clear();
   t.insert(infected.begin(), infected.end());
+
+  init();
+  edge_length.clear();
   distance.clear();
   std::cin >> m;
   for (int i=0;i<m;i++){
