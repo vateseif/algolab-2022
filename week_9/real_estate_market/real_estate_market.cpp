b16cb339f536373dadc97565d47ad2a08ffebb27
vateseif <vateseif@gmail.com>
2022-12-12 18:31:53 +0100

    real estate market 80. too slow for tc 5. may need reformulation to go from negative to positive costs

---
+++ week_9/real_estate_market/real_estate_market.cpp
@@ -0,0 +1,85 @@
+#include<iostream>
+// BGL includes
+#include <boost/graph/adjacency_list.hpp>
+#include <boost/graph/cycle_canceling.hpp>
+#include <boost/graph/push_relabel_max_flow.hpp>
+#include <boost/graph/successive_shortest_path_nonnegative_weights.hpp>
+#include <boost/graph/find_flow_cost.hpp>
+
+// Graph Type with nested interior edge properties for Cost Flow Algorithms
+typedef boost::adjacency_list_traits<boost::vecS, boost::vecS, boost::directedS> traits;
+typedef boost::adjacency_list<boost::vecS, boost::vecS, boost::directedS, boost::no_property,
+    boost::property<boost::edge_capacity_t, long,
+        boost::property<boost::edge_residual_capacity_t, long,
+            boost::property<boost::edge_reverse_t, traits::edge_descriptor,
+                boost::property <boost::edge_weight_t, long> > > > > graph; // new! weightmap corresponds to costs
+typedef traits::vertex_descriptor vertex_desc;
+typedef boost::graph_traits<graph>::edge_descriptor             edge_desc;
+typedef boost::graph_traits<graph>::out_edge_iterator           out_edge_it; // Iterator
+
+// Custom edge adder class
+class edge_adder {
+ graph &G;
+
+ public:
+  explicit edge_adder(graph &G) : G(G) {}
+  void add_edge(int from, int to, long capacity, long cost) {
+    auto c_map = boost::get(boost::edge_capacity, G);
+    auto r_map = boost::get(boost::edge_reverse, G);
+    auto w_map = boost::get(boost::edge_weight, G); // new!
+    const edge_desc e = boost::add_edge(from, to, G).first;
+    const edge_desc rev_e = boost::add_edge(to, from, G).first;
+    c_map[e] = capacity;
+    c_map[rev_e] = 0; // reverse edge has no capacity!
+    r_map[e] = rev_e;
+    r_map[rev_e] = e;
+    w_map[e] = cost;   // new assign cost
+    w_map[rev_e] = -cost;   // new negative cost
+  }
+};
+
+void testcase(){
+  int N, M, S;
+  std::cin >> N >> M >> S;
+
+  // Create graph, edge adder class and propery maps
+  graph G(S+M+N);
+  const vertex_desc v_source = boost::add_vertex(G);
+  const vertex_desc v_sink = boost::add_vertex(G);
+  edge_adder adder(G); 
+
+  for (int i=0;i<S;i++){
+    int li;
+    std::cin>>li;
+    adder.add_edge(v_source, i, li, 0);
+  }
+
+  for (int i=0;i<M;i++){
+    int si;
+    std::cin>>si;
+    adder.add_edge(si-1, S+i, 1, 0);
+  }
+
+  for (int i=0;i<N;i++){
+    for (int j=0;j<M;j++){
+      int bij;
+      std::cin>>bij;
+      adder.add_edge(S+j, S+M+i, 1, -bij);
+    }
+    adder.add_edge(S+M+i, v_sink, 1, 0);
+  }
+
+  int flow = boost::push_relabel_max_flow(G, v_source, v_sink);
+  boost::cycle_canceling(G);
+  int cost = -boost::find_flow_cost(G);
+  std::cout << flow << " " << cost << std::endl; 
+
+}
+
+
+int main(){
+  std::ios_base::sync_with_stdio(false);
+  int T; std::cin>>T;
+  while(T--) testcase();
+  return 0;
+}
