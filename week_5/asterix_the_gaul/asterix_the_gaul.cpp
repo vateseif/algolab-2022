c17da2ed3cb301781e0bff0f1e1faa28ccff3892
Seif <vateseif@gmail.com>
2023-01-02 21:31:48 +0100

    asterix the gaul 60. too slow for tc 4

--- week_5/asterix_the_gaul/asterix_the_gaul.cpp
+++ week_5/asterix_the_gaul/asterix_the_gaul.cpp
@@ -1,26 +1,41 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <limits> 
 
 #define trace(x) std::cerr << #x << " = " << x << std::endl;
 
-
+std::vector<long> potions;
+std::vector<long> memo_distance; // stores max distance reached for each # of movements used
 std::vector<std::pair<int, int>> M;
 
-bool isCaptured(int& n, long& D, long& T){
+int min_gulps(int& n, int& m, long& D, long& T){
+  memo_distance.clear(); 
+  memo_distance.resize(n, std::numeric_limits<long>::min());
   for (int s = 0; s < 1<<n; ++s) { // Iterate through all subsets
     long sum_t = 0;
     long sum_d = 0;
+    int n_movements = 0;
     for (int i = 0; i < n; ++i) {
       // If i-th element in subset
       if (s & 1<<i) {
         sum_d += M[i].first;
         sum_t += M[i].second;
+        n_movements++;
       }
-      if (sum_d>=D && sum_t<T) return false;
+      if (sum_d>=D && sum_t<T) return 0;
+    }
+    if (sum_t<T) memo_distance[n_movements] = std::max(memo_distance[n_movements], sum_d);
+  }
+
+  for (int i=0; i<m; i++){
+    long si = potions[i];
+    for (int n_mov=0; n_mov<n; n_mov++){
+      if (memo_distance[n_mov] + n_mov*si >= D) return i+1;
     }
   }
-  return true;
+
+  return -1;
 }
 
 
@@ -36,15 +51,19 @@ void testcase(){
     M[i] = std::make_pair(d, t);
   }
 
+  potions.clear();
+  potions.resize(m); 
   for (int i=0; i<m; i++){
     long s;
     std::cin >> s;
+    potions[i] = s;
   }
 
-  if (isCaptured(n, D, T)){
+  int gulps = min_gulps(n, m, D, T);
+  if (gulps == -1){
     std::cout << "Panoramix captured" << std::endl;
   }else{
-    std::cout << 0 << std::endl;
+    std::cout << gulps << std::endl;
   }
   
 
