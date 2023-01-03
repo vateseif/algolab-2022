12bb38794e28dea3a5f4a753b951d0a98d0f94f7
Seif <vateseif@gmail.com>
2023-01-03 04:39:09 +0100

    shopping trip 100

---
+++ week_7/shopping_trip/shopping_trip.cpp
@@ -0,0 +1,77 @@
+///1
+#include <iostream>
+#include <vector>
+#include <string>
+// BGL include
+#include <boost/graph/adjacency_list.hpp>
+// BGL flow include *NEW*
+#include <boost/graph/push_relabel_max_flow.hpp>
+
+#define trace(x) std::cerr << #x << " = " << x << std::endl;
+
+// Graph Type with nested interior edge properties for flow algorithms
+typedef boost::adjacency_list_traits<boost::vecS, boost::vecS, boost::directedS> traits;
+typedef boost::adjacency_list<boost::vecS, boost::vecS, boost::directedS, boost::no_property,
+    boost::property<boost::edge_capacity_t, long,
+        boost::property<boost::edge_residual_capacity_t, long,
+            boost::property<boost::edge_reverse_t, traits::edge_descriptor>>>> graph;
+
+typedef traits::vertex_descriptor vertex_desc;
+typedef traits::edge_descriptor edge_desc;
+typedef boost::graph_traits<graph>::out_edge_iterator out_edge_it;
+
+// Custom edge adder class, highly recommended
+class edge_adder {
+  graph &G;
+
+ public:
+  explicit edge_adder(graph &G) : G(G) {}
+
+  void add_edge(int from, int to, long capacity, long rev_capacity) {
+    auto c_map = boost::get(boost::edge_capacity, G);
+    auto r_map = boost::get(boost::edge_reverse, G);
+    const auto e = boost::add_edge(from, to, G).first;
+    const auto rev_e = boost::add_edge(to, from, G).first;
+    c_map[e] = capacity;
+    c_map[rev_e] = rev_capacity; // reverse edge has no capacity!
+    r_map[e] = rev_e;
+    r_map[rev_e] = e;
+  }
+};
+
+void testcase(){
+  int n, m, s;
+  std::cin >> n >> m >> s;
+  // edge adder
+  graph G(n);
+  edge_adder adder(G);
+  // Add special vertices source and sink
+  const vertex_desc v_sink = boost::add_vertex(G);
+
+
+  for (int i=0; i<s; i++){
+    long si; std::cin >> si;
+    adder.add_edge(si, v_sink, 1, 0);
+  }
+
+  for (int i=0; i<m; i++){
+    long u, v;
+    std::cin >> u >> v;
+    adder.add_edge(u, v, 1, 0);
+    adder.add_edge(v, u, 1, 0);
+  }
+
+  long flow = boost::push_relabel_max_flow(G, 0, v_sink);
+  std::string out = flow==s? "yes" : "no";
+  std::cout << out << "\n";
+  
+  return;
+}
+
+
+int main(){
+  std::ios_base::sync_with_stdio(false);
+  int T; std::cin >> T;
+  while(T--) testcase();
+  return 0;
+}
