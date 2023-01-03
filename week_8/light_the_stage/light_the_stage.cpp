1bee3560accf21abbfee5cc6a1e411f4b35d69cc
Seif <vateseif@gmail.com>
2023-01-03 17:27:53 +0100

    light the stage 60

---
+++ week_8/light_the_stage/light_the_stage.cpp
@@ -0,0 +1,75 @@
+///2
+#include <iostream>
+#include <vector>
+#include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
+#include <CGAL/Delaunay_triangulation_2.h>
+
+#define trace(x) std::cerr << #x << " = " << x << std::endl;
+
+typedef CGAL::Exact_predicates_inexact_constructions_kernel K;
+typedef CGAL::Delaunay_triangulation_2<K> Triangulation;
+typedef K::Point_2 P;
+
+struct Player {
+  P position;
+  long r;
+  // This constructor is convenient for a concise initialisation. It can also
+  // be omitted and the member variables be set manually.
+  //Player(P pos, long radius) : position(pos), r(radius) {}
+};
+
+
+std::vector<P> lamps;
+std::vector<int> winners;
+std::vector<Player> players;
+
+void testcase(){
+  int m, n;
+  std::cin >> m >> n;
+
+  // store players as Player
+  players.clear(); players.resize(m);
+  for (int i=0; i<m; i++){
+    int x, y;
+    long r;
+    std:: cin >> x >> y >> r;
+    Player p;
+    p.r = r;
+    p.position = P(x, y);
+    players[i] = p;
+  }
+  // compute squared radius of lamp area
+  long h; std::cin >> h;
+  long lamp_r_squared = h * h;
+
+  // load lamps positions
+  lamps.clear(); lamps.resize(n);
+  for (int i=0; i<n; i++){
+    int x, y;
+    std::cin >> x >> y;
+    lamps[i] = P(x, y);
+  }
+  // create triangulation of lamp positions
+  Triangulation t;
+  t.insert(lamps.begin(), lamps.end());
+
+  int i = 0;
+  for (Player p : players){
+    Triangulation::Vertex_handle nearest_lamp = t.nearest_vertex(p.position);
+    long squared_d = CGAL::squared_distance(nearest_lamp->point(), p.position);
+    if (p.r*p.r + lamp_r_squared + 2*h*p.r <= squared_d) {
+      std::cout << i << " ";
+    }
+    i++;
+  }
+  std::cout << std::endl;
+
+  return;
+}
+
+int main(){
+  std::ios_base::sync_with_stdio(false);
+  int T; std::cin>>T;
+  while(T--) testcase();
+  return 0;
+}
