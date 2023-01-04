17328d5b0bd83f25c0e92d749dfa8d69a9bcaa55
Seif <vateseif@gmail.com>
2023-01-04 14:29:21 +0100

    casino royale 100

--- week_9/casino_royale/casino_royale.cpp
+++ week_9/casino_royale/casino_royale.cpp
@@ -1,3 +1,4 @@
+///1
 #include <iostream>
 #include <limits>
 // BGL includes
@@ -18,6 +19,7 @@ typedef boost::adjacency_list<boost::vecS, boost::vecS, boost::directedS, boost:
 typedef traits::vertex_descriptor                               vertex_desc;
 typedef boost::graph_traits<graph>::edge_descriptor             edge_desc;
 typedef boost::graph_traits<graph>::out_edge_iterator           out_edge_it; // Iterator
+typedef boost::graph_traits<graph>::edge_iterator               edge_it;
 
 // Custom edge adder class
 class edge_adder {
@@ -41,7 +43,7 @@ class edge_adder {
 };
 
 
-int MAX_VALUE = std::numeric_limits<int>::max();
+int MAX_VALUE = 128; // max priority of each mission
 
 void testcase(){
   int n, m, l;
@@ -50,30 +52,23 @@ void testcase(){
   // init graph
   graph G(n);
   edge_adder adder(G);
-  auto c_map = boost::get(boost::edge_capacity, G);
-  auto r_map = boost::get(boost::edge_reverse, G);
-  auto rc_map = boost::get(boost::edge_residual_capacity, G);
   // Add special vertices source and sink
   const vertex_desc v_source = boost::add_vertex(G);
 
   // init graph edges with max flow being train limit
   adder.add_edge(v_source, 0, l, 0);
-  for (int i=0; i<n-1; i++) adder.add_edge(i, i+1, l, 0);
+  for (int i=0; i<n-1; i++) adder.add_edge(i, i+1, l, MAX_VALUE);
   // create edges representing missions
   for (int i=0; i<m; i++){
     int x, y, q;
     std::cin >> x >> y >> q;
-    adder.add_edge(x, y, 1, -q);
+    adder.add_edge(x, y, 1, (y-x)*MAX_VALUE - q);
   }
 
   // solve mincut/maxflow
-  int flow = boost::push_relabel_max_flow(G, v_source, n-1);
-  boost::cycle_canceling(G);
-  int cost = boost::find_flow_cost(G);
-  std::cout << -cost << "\n";
-  
-
-
+  boost::successive_shortest_path_nonnegative_weights(G, v_source, n-1);
+  long cost = boost::find_flow_cost(G);
+  std::cout << MAX_VALUE*l*(n-1) - cost << std:: endl;
   return;
 }
 
