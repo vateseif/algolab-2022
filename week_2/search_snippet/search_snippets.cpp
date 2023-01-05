222c3eacf7150e16c889103962c64887bdb98b73
Seif <vateseif@gmail.com>
2023-01-05 03:06:53 +0100

    search snippets 100

--- week_2/search_snippet/search_snippets.cpp
+++ week_2/search_snippet/search_snippets.cpp
@@ -3,59 +3,58 @@
 #include <vector>
 #include <limits>
 #include <cmath>
+#include <unordered_set>
+#include <unordered_map>
 #include <algorithm>
 
+#define trace(x) std::cerr << #x << " = " << x << std::endl;
 
-std::vector<int> word_number; // number of times each word occurs in document
-std::vector<std::vector<int>> word_location; // stores m locations of the n words
+std::vector<int> word_number, word_counter; // number of times each word occurs in document
+std::vector<std::pair<int, int>> document;
+
 
 void testcase(){
   int n; std::cin >> n;
-
+  int N=0; // total number of word positions given
   word_number.clear();
-  word_location.clear();
   word_number.resize(n);
-  word_location.resize(n);
   for  (int i=0;i<n; i++){
     int mi; std::cin>>mi;
     word_number[i] = mi;
-    word_location[i].resize(mi);
+    N+=mi;
   }
   
-  int a = 0;
-  int b = std::pow(2, 30);
-  
+  document.clear();
+  document.resize(N);
+  int c = 0;
   for (int i=0; i<n; i++){
     int mi = word_number[i];
     for (int j=0; j<mi; j++){
       int pij; std::cin >> pij;
-      word_location[i][j] = pij;
-      if (i>0){
-        auto it_closest = std::upper_bound(word_location[i-1].begin(), word_location[i-1].end(), pij);
-        if (it_closest == word_location[i-1].end()){
-          it_closest--;
-          int p_previous = *it_closest;
-          if (pij - p_previous < b-a){
-            a = p_previous;
-            b = pij;
-          }
-        }else{
-          if (*it_closest - pij < b-a){
-            a = pij;
-            b = *it_closest;
-          }
-          if (it_closest==word_location[i-1].begin()) continue;
-          it_closest--;
-          if (pij - *it_closest < b-a){
-            a = *it_closest;
-            b = pij;
-          }
-        }
-      }
+      document[c] = std::make_pair(pij, i);
+      c++;
     }
   }
-  std::cout << b-a+1 << std::endl;
+
+  std::sort(document.begin(), document.end());
+
+  int back = 0;
+  int missing_words = n;
+  int min_d = std::numeric_limits<int>::max();
+  word_counter.clear(); word_counter.resize(n, 0);
+  for (int i=0; i<N; i++){
+    if (!word_counter[document[i].second]++){
+      missing_words--;
+    }
+    while (missing_words==0){
+      min_d = std::min(min_d, document[i].first-document[back].first+1);
+      if (!--word_counter[document[back++].second]){
+        missing_words++;
+      }  
+    }
+  }
   
+  std::cout << min_d << std::endl;
   return;
 }
 
