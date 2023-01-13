3346ea1fd0601252fbf391c2f80986ca4fc2e9d4
Seif <vateseif@gmail.com>
2023-01-13 03:08:25 +0100

    new york 100

--- week_10/new_york/new_york.cpp
+++ week_10/new_york/new_york.cpp
@@ -1,86 +1,113 @@
-///2
+///3
 #include <iostream>
 #include <vector>
-#include <algorithm>
+#include <algorithm> 
 #include <deque>
 #include <map>
-#include <set>
 
 #define trace(x) std::cerr << #x << " = " << x << std::endl;
 
 int n, k;
 size_t m;
-
-std::vector<int> temp, critical; // route and temp for each island
-std::vector<std::vector<int>> routes;
+std::vector<int> temp, critical;
+std::vector<std::vector<int>> edges;
 std::map<int, bool> critical_map;
 
-void update_window(std::deque<int>& window, std::map<int, int>& window_temp){
-  // check if front node is a critical node
-  int min_h = window_temp.begin()->first;
-  int max_h = window_temp.rbegin()->first;
-  if (!critical_map[window.front()] && max_h - min_h <= k) {
-    critical.push_back(window.front());
-    critical_map[window.front()] = true; 
+
+int update_window(std::deque<int>& window, std::map<int, int>& window_temp){
+  int front = window.front();
+  // check if front is a critical point
+  if (!critical_map[front]){
+    int min_h = window_temp.begin()->first;
+    int max_h = window_temp.rbegin()->first;
+    if (max_h-min_h<=k) {
+      critical_map[front] = true;
+      critical.push_back(front);
+    }
   }
-  //if (max_h - min_h <= k) critical_set.insert(window.front());
-  // remove counter of temperature of front node is the count becomes 0
-  if (!(--window_temp[temp[window.front()]]))
-    window_temp.erase(window_temp.find(temp[window.front()]));
-  // remove front node
+  // remove front element
   window.pop_front();
+  // update temperature count (remove in case temp becomes 0)
+  if (!(--window_temp[temp[front]]))
+    window_temp.erase(window_temp.find(temp[front]));
+    
+  return front;
 }
 
-void sliding_window(std::deque<int> window, std::map<int, int> window_temp,int v){
-  // add new node to window
-  window.push_back(v);
-  window_temp[temp[v]]++;
-  // check for critical window and remove oldest
+void reset_window(int front, std::deque<int>& window, std::map<int, int>& window_temp){
+  if (front!=-1){
+    // add back to front the original node
+    window.push_front(front);
+    window_temp[temp[front]]++;
+  }
+  // remove latest node added at the back
+  // update temperature count (remove in case temp becomes 0)
+  if (!(--window_temp[temp[window.back()]]))
+    window_temp.erase(window_temp.find(temp[window.back()]));
+  // remove node
+  window.pop_back();
+}
+
+
+void dp(int u, std::deque<int>& window, std::map<int, int>& window_temp){
+  // store front element of window to be readded
+  int front = -1;
+  // insert new element
+  window.push_back(u);
+  window_temp[temp[u]]++;
+  // remove top and check if critical point
   if (window.size()==m){
-    update_window(window, window_temp);
+    front = update_window(window, window_temp);
   }
-  // add new node
-  for (int next_v : routes[v]){
-    sliding_window(window, window_temp, next_v);
-    //if (critical_map[v]) break;
+  // carry on with neighbors
+  for (int v : edges[u]){
+    dp(v, window, window_temp);
   }
+  // reset window: remove newest element and readd oldeest
+  reset_window(front, window, window_temp);
 }
-
 
 void testcase(){
   std::cin >> n >> m >> k;
+
   temp.clear();
   temp.resize(n);
   for (int i=0; i<n; i++){
-    int hi; std::cin >> hi;
-    temp[i] = hi;
+    int t; std::cin >> t;
+    temp[i] = t;
   }
 
-  routes.clear();
-  routes.resize(n);
+  edges.clear();
+  edges.resize(n);
   for (int i=0; i<n-1; i++){
     int u, v;
     std::cin >> u >> v;
-    routes[u].push_back(v);
+    edges[u].push_back(v);
   }
-  
+
   critical.clear();
   critical_map.clear();
   std::deque<int> window;
   std::map<int, int> window_temp;
-  sliding_window(window, window_temp, 0);
-
   
+  dp(0, window, window_temp);
+
+
   if (critical.size()>0){
     std::sort(critical.begin(), critical.end());
-    for (int s : critical) std::cout  << s << " ";
-  } else{
-    std::cout  << "Abort mission";
+    for (int u : critical) {
+      std::cout << u << " ";
+    }
+  }else{
+    std::cout << "Abort mission";
   }
-  std::cout  << std::endl;
+  
+  std::cout << std::endl;
 
   return;
 }
+
+
 
 int main(){
   std::ios_base::sync_with_stdio(false);
