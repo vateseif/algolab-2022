d00d8667cbeed4004826b307baa364aa36df627f
vateseif <vateseif@gmail.com>
2023-01-30 20:38:57 +0100

    boats desc

---
+++ week_5/boats/README.md
@@ -0,0 +1,16 @@
+# Boats
+
+Tags: greedy
+
+## Problem
+We are given for `n` boats a position `p_i` and length `l_i`. We need to find the max number of boat such that not boats overlap and the position `p_i` is located within the boat length.
+
+## Solution
+The problem can be solved greedily.
+
+We sort all boats increasingly by their position `p_i`. We iterate through each boat storing 2 values: the rightmost point of the last added boat `r_prev` and the rightmost point of the current boat we're considering adding `r`.
+The rightmost point is defined as `r_i = max(p_i, r_prev+l_i)`.
+
+If `r_i > p_j` then we know that we can't add both boat `i` and `j` since it would violate our constraint. We then need to consider adding only `i` or `j`. We take the one whose rightmost point is the smallest. Thus we update `r = min(r_i, r_j)`.
+
+If `r_i <= p_j` we know we can safely add boat `i` without constraint violation. Then we upadted `r_prev = r` and `r = r_j` and check whether to add boat `j`.
--- week_5/boats/boats.cpp
+++ week_5/boats/boats.cpp
@@ -1,74 +1,59 @@
 ///1
-#include<iostream>
-#include<vector>
-#include<algorithm>
-#include<limits>
+#include <iostream>
+#include <vector>
+#include <limits>
+#include <algorithm>
 
 #define trace(x) std::cerr << #x << " = " << x << std::endl;
 
 int n;
-std::vector<int> l, p;                    // store boat lengths and ring position
-std::vector<std::pair<int, int>> rings;   // store boat lengths and ring position
+std::vector<std::pair<long, long>> boats;
 
-
-bool greedy(int& delta, int& boats, int& i){
-  /*
-  Find earliest boat finish that does not collide with finish location delta of previous boat.
-  */
-  const int ii = i;
-  int min_e = std::numeric_limits<int>::max();
-  for (int j=ii+1; j<n; j++){
-    if (p[j] > min_e) break;  // current earlieast finish < ring node j <= earliest finish node j
-    if (delta <= p[j]){       // ensures boat j can be placed within ring
-      int e = std::max(p[j], delta+l[j]);
-      if (min_e > e){
-        min_e = e;
-        i = j;
-      } 
-    }
+void dp(int& i, int& counter, long& r, long& r_prev){
+  if (i==n) return;
+  std::pair<long, long> boat = boats[i++];
+  // if right side of previous boat covers pi we pick boat with smallest r
+  if (r > boat.first){
+    long r_new = std::max(boat.first, r_prev+boat.second);
+    r = std::min(r, r_new);
+    dp(i, counter, r, r_prev);
+  }else{
+    r_prev = r;
+    r = std::max(boat.first, r+boat.second);
+    counter++;
+    dp(i, counter, r, r_prev);  
   }
-  if (min_e == std::numeric_limits<int>::max()) return false;
-  delta = min_e;
-  boats++;
-  return true;
 }
+
 
 void testcase(){
   std::cin >> n;
-  rings.clear();
-  rings.resize(n);
+
+  // load boats
+  boats.clear(); boats.resize(n);
   for (int i=0; i<n; i++){
-   int li, pi;
-   std::cin >> li >> pi;
-   rings[i] = std::make_pair(pi, li);
-  }
-  std::sort(rings.begin(), rings.end()); // pair is sorted lexicographically so p must go first
-  
-  l.clear(); l.resize(n);
-  p.clear(); p.resize(n);
-  for (int i=0; i<n; i++){
-    p[i] = rings[i].first;
-    l[i] = rings[i].second;
+    long li, pi;
+    std::cin >> li >> pi;
+    boats[i] = std::make_pair(pi, li);
   }
 
-  /*
-  greedy algorithm: for each boat, move it to the most left possible (no collision and within ring location)
-  then take local optimum of the earliest boat finish
-  */
-  int i = -1;
-  int boats = 0;
-  int delta = std::numeric_limits<int>::min();
-  while(i < n-1){
-    if (!greedy(delta, boats, i)) break;
-  }
+  // sort boats increasingly on location of p
+  std::sort(boats.begin(), boats.end());
 
-  std::cout << boats << std::endl;
+  int i = 0;
+  int counter = 0;
+  long r = std::numeric_limits<long>::min();
+  long r_prev = std::numeric_limits<long>::min();
+
+  dp(i, counter, r, r_prev);
+  std::cout << counter << std::endl;
 }
+
 
 
 int main(){
   std::ios_base::sync_with_stdio(false);
   int T; std::cin >> T;
-  while(T--) testcase();
+  while (T--) testcase();
   return 0;
 }
